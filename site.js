9a017d109e9149355256ab520dea4e3774b209a5
Andrew Jackson <andrew@disvelop.net>
2019-02-21 12:59:39 +1100

    Added dark/light mode option

--- site.js
+++ site.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
+    loadStyle();
     getPlayerData().then(x => updateContent(x)).catch(x => console.log(x));
+
+    document.getElementById("lightlink").addEventListener('click', setLightMode, true);
+    document.getElementById("darklink").addEventListener('click', setDarkMode, true);
 }, false);
 
 
 async function getPlayerData() {
-    let request = await fetch('/api/list');
+    let request = await fetch('https://nplist.now.sh/api/list');
     if (request.status == 200) {
         let data = await request.json();
         return data;
@@ -25,46 +29,98 @@ function setTitle(data) {
 }
 
 function createPlayersTable(data) {
-    data.players = data.players.sort((a,b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0)); 
+    data.players = data.players.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0));
     data.players.forEach(function (player, i) {
 
-        newRow   = table.insertRow(table.rows.length);
-        newCell  = newRow.insertCell(0);          
-        newText  = document.createTextNode(`${i+1}`)
-        newCell.appendChild(newText);
-        
-        newCell  = newRow.insertCell(1);          
-        newText  = document.createTextNode(`${player.id}`)
+        newRow = table.insertRow(table.rows.length);
+        newCell = newRow.insertCell(0);
+        newText = document.createTextNode(`${i+1}`)
         newCell.appendChild(newText);
 
-        newCell  = newRow.insertCell(2);          
+        newCell = newRow.insertCell(1);
+        newText = document.createTextNode(`${player.id}`)
+        newCell.appendChild(newText);
+
+        newCell = newRow.insertCell(2);
         a = document.createElement('a');
-        a.setAttribute('href',`https://steamcommunity.com/profiles/${player.identifiers[2]}`);
-        a.innerHTML = player.name;   
+        a.setAttribute('href', `https://steamcommunity.com/profiles/${player.identifiers[2]}`);
+        a.innerHTML = player.name;
         newCell.appendChild(a);
 
-        newCell  = newRow.insertCell(3);
+        newCell = newRow.insertCell(3);
         a = document.createElement('a');
-        a.setAttribute('href',`http://www.nopixel.net/upload/index.php?members/${player.identifiers[4]}`);
-        a.innerHTML = player.identifiers[0];       
+        a.setAttribute('href', `http://www.nopixel.net/upload/index.php?members/${player.identifiers[4]}`);
+        a.innerHTML = player.identifiers[0];
         newCell.appendChild(a);
 
-        newCell  = newRow.insertCell(4);          
-        newText  = document.createTextNode(`${player.identifiers[1]}`)
+        newCell = newRow.insertCell(4);
+        newText = document.createTextNode(`${player.identifiers[1]}`)
         newCell.appendChild(newText);
 
-        newCell  = newRow.insertCell(5);          
-        newText  = document.createTextNode(`${player.identifiers[2]}`)
+        newCell = newRow.insertCell(5);
+        newText = document.createTextNode(`${player.identifiers[2]}`)
         newCell.appendChild(newText);
 
-        newCell  = newRow.insertCell(6);          
+        newCell = newRow.insertCell(6);
         a = document.createElement('a');
-        a.setAttribute('href',`http://twitch.tv/${player.identifiers[3]}`);
-        a.innerHTML = player.identifiers[3];       
+        a.setAttribute('href', `http://twitch.tv/${player.identifiers[3]}`);
+        a.innerHTML = player.identifiers[3];
         newCell.appendChild(a);
 
-        newCell  = newRow.insertCell(7);          
-        newText  = document.createTextNode(`${player.ping}`)
+        newCell = newRow.insertCell(7);
+        newText = document.createTextNode(`${player.ping}`)
         newCell.appendChild(newText);
     });
 }
+
+function loadStyle() {
+
+    style = localStorage.getItem('style');
+
+    if (style === 'dark') {
+
+        link = document.createElement("link");
+        link.href = "https://unpkg.com/bulmaswatch/darkly/bulmaswatch.min.css";
+        link.type = "text/css";
+        link.id = "darkmode"
+        link.rel = "stylesheet";
+        document.getElementsByTagName("head")[0].appendChild(link);
+    }
+
+
+    link = document.createElement("link");
+    link.href = "site.css";
+    link.type = "text/css";
+    link.id = "sitestyle"
+    link.rel = "stylesheet";
+
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
+
+function setDarkMode(e) {
+    e.preventDefault();
+    darkmode = document.getElementById('darkmode'); 
+    if(darkmode) {
+        return
+    }
+    localStorage.setItem('style', 'dark')
+    loadStyle();
+}
+
+
+function setLightMode(e) {
+    e.preventDefault();
+    localStorage.removeItem('style')
+
+    darkmode = document.getElementById('darkmode');
+
+    if (darkmode) {
+        darkmode.disabled = true;
+        darkmode.parentNode.removeChild(darkmode);
+
+        darkmode = document.getElementById('sitestyle');
+        darkmode.disabled = true;
+        darkmode.parentNode.removeChild(darkmode);
+    }
+    loadStyle();
+} 
